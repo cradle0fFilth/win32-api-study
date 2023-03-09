998b02ed72ac6f6d75147c2363d0bd1965cf59af
cradle0fFilth <64477775+cradle0fFilth@users.noreply.github.com>
2023-03-09 20:22:03 +0800

    use parameters

--- create-thread/create-thread/main.cpp
+++ create-thread/create-thread/main.cpp
@@ -2,16 +2,23 @@
 #include<Windows.h>
 
 DWORD WINAPI ThreadProc(LPVOID lpParameter) { //线程函数格式
-	for (int i = 0; i < 100; i++) {
+	int* p = (int*)lpParameter;
+	for (int i = 0; i < *p; i++) {
 		Sleep(500);
 		printf("0000000000000%d \n", i);
 	}
 	return 0;
 }
+void WINAPI TestThread() { //线程函数格式
+	for (int i = 0; i < 100; i++) {
+		Sleep(500);
+		printf("0000000000000%d \n", i);
+	}
+}
 
 int main(int argc,char* argv[]) {
-
-	HANDLE hThread=CreateThread(NULL, 0, ThreadProc, NULL, 0, NULL);
+	int n = 10;
+	HANDLE hThread=CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)ThreadProc, &n, 0, NULL);
 	CloseHandle(hThread);
 
 	for (int i = 0; i < 100; i++) {
