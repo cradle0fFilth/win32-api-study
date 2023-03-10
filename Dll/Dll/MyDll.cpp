a193a557cf439284baa0851299bee9fbfecf4fad
cradle0fFilth <64477775+cradle0fFilth@users.noreply.github.com>
2023-03-10 12:53:57 +0800

    Fix problem

--- Dll/Dll/MyDll.cpp
+++ Dll/Dll/MyDll.cpp
@@ -1,9 +1,13 @@
-#include "MyDll.h"
+
 #include "pch.h"
 
-int _stdcall Plus(int x, int y) {
+extern "C" _declspec(dllexport)int _stdcall Plus(int x, int y);
+
+extern "C" _declspec(dllexport)int _stdcall  Sub(int x, int y);
+
+int  _stdcall Plus(int x, int y) {
 	return x + y;
 }
-int _stdcall  Sub(int x, int y) {
+int _stdcall Sub(int x, int y) {
 	return x - y;
 }
--- Dll/Dll/MyDll.h
+++
@@ -1,6 +0,0 @@
-
-extern "C" _declspec(dllexport)int _stdcall Plus(int x, int y);
-extern "C" _declspec(dllexport)int _stdcall  Sub(int x, int y);
-int Plus(int x, int y);
-
-int Sub(int x, int y);
