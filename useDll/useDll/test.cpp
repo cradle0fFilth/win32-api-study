64eb6ccf3d32ac46ca5494a269301f15cbc2a4c5
cradle0fFilth <64477775+cradle0fFilth@users.noreply.github.com>
2023-03-10 13:37:39 +0800

    use the dll

---
+++ useDll/useDll/test.cpp
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<Windows.h>
+//定义函数指针
+typedef int(_stdcall* lpPlus)(int, int);
+typedef int(_stdcall* lpSub)(int, int);
+ 
+//声明函数指针变量
+lpPlus myPlus;
+lpSub mySub;
+
+//Dll必须加载到当前进程空间
+int main() {
+
+	HINSTANCE hModule = LoadLibrary(L"Dll.dll");
+
+	if (hModule == NULL) {
+		printf("加载dll错误\n");
+	}
+	else {
+		printf("成功加载dll\n");
+	}
+
+	myPlus = (lpPlus)GetProcAddress(hModule, "Plus");  //得到模块的函数地址
+	mySub = (lpSub)GetProcAddress(hModule, "Sub");
+
+	if (myPlus == NULL) {
+		printf("加载函数失败 \n");
+	}
+	else {
+		printf("加载函数成功 \n");
+	}
+
+	int x = myPlus(1, 2);
+	int y = mySub(5, 2);
+	printf("x = %d \ny = %d \n", x, y);
+
+	return 0;
+}
